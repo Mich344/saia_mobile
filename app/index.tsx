22d507a1412732ace58c3922ce40214c4290c364
Camilo-Cadenas <tec.sistemas.camilo@gmail.com>
2026-04-29 00:35:30 -0500

    modificacion en mensaje de welcome

--- app/index.tsx
+++ app/index.tsx
@@ -3,7 +3,7 @@ import { View, Text } from "react-native";
 export default function page() {
   return (
     <View className="flex-1 justify-center items-center bg-red-500">
-      <Text className="text-red text-xl color-complement">Welcome to SAIA</Text>
+      <Text className="text-red text-xl color-complement">Welcome to SAIA!</Text>
     </View>
   );
 }
