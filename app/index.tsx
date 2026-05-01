9bac3a60ae106938b8ffcbb7fc7862690e067c63
emanuelmendoza-a11y <emanuelmendozafili@gmail.com>
2026-04-30 19:07:13 -0500

    crea pantalla login

--- app/index.tsx
+++ app/index.tsx
@@ -1,11 +1,20 @@
-import { View, Image } from "react-native";
+import { View, Image, Text, TextInput } from "react-native";
+
+// Librerias
 
 // Componentes
 import Input from "@/components/Inputs";
+import ColorGrandient from "@/components/GradientP";
+
 import Container from "@/components/Container";
 
 export default function page() {
   return (
+<<<<<<< HEAD
+    <View className="flex-1 justify-center items-center bg-red-500">
+      <Text className="text-red text-xl color-complement">Welcome to SAIA xd xd!</Text>
+    </View>
+=======
     <Container>
       <View className="items-center pt-10">
         <View>
@@ -13,12 +22,13 @@ export default function page() {
             className="w-[150px] h-[163px]"
             source={require("@/img/logo_SAIA.png")}
           />
+          {/* <ColorGrandient text="INICIAR SESIÓN" /> */}
         </View>
       </View>
-
       <View className="gap-[50px]">
         <Input placeholder={"Numero de documento *"} />
       </View>
     </Container>
+>>>>>>> d27aed4a7f85c31b67c3bca4e4d283bb94135f1f
   );
 }
